98653da0ae16f8d10ef42612f230b20f684eb7a7
Filip Gralinski <filipg@amu.edu.pl>
2021-05-01 19:06:46 +0200

    Fix setting locale

--- {{cookiecutter.paper_repo_name}}/autozoil-via-docker.sh
+++ {{cookiecutter.paper_repo_name}}/autozoil-via-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v $(pwd):/link  -it loxygen/autozoil /opt/autozoil/autozoil --locale en_US /link/main.tex --alt-log-file /link/{{cookiecutter.paper_id}}.log
+docker run -v $(pwd):/link  -it loxygen/autozoil /opt/autozoil/autozoil --locale {{cookiecutter.locale}} /link/main.tex --alt-log-file /link/{{cookiecutter.paper_id}}.log
