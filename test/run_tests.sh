b6ef23a6d0411bcaaf18e877898c7f87b587dba2
Filip Gralinski <filipg@amu.edu.pl>
2022-12-20 21:23:57 +0100

    Add test for English amuthesis

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -16,7 +16,7 @@ check()
     cd "$PAPER_DIR"
     make clean
     make
-    if [[ "$config_file" != "configs/pw-thesis.yml" && "$config_file" != "configs/amu.yml" && "$config_file" != "configs/ieee-access.yml" ]]
+    if [[ "$config_file" != "configs/pw-thesis.yml" && "$config_file" != "configs/amu.yml" && "$config_file" != "configs/ieee-access.yml" && "$config_file" != "configs/amu-en.yml" ]]
     then
         make arxiv-$PAPER_ID.tar.gz
     fi
